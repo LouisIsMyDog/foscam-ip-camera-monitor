7856f45589ca106e40a183fddce06733c183a4c9
Emre <11022261+LouisIsMyDog@users.noreply.github.com>
2020-10-03 18:18:33 -0400

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 //Database Constants
 defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
 defined('DB_USER') ? null : define("DB_USER", "admin_ardor");
-defined('DB_PASS') ? null : define("DB_PASS", "mahmut88");
+defined('DB_PASS') ? null : define("DB_PASS", "");
 defined('DB_NAME') ? null : define("DB_NAME", "admin_camerardor");
 defined('DB_PORT') ? null : define("DB_PORT", "3306");
 
